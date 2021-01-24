e3f7a43fd3e0b4e5ec206d26b7dc5ebe73b7591d
Serhii <furkalokbu@gmail.com>
2021-01-24 19:08:08 +0200

    создал проект, добавил новое приложение

---
+++ First_API_Django/onlinestore/products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    manufacturer = models.ForeignKey(
+        Manufacturer, on_delete=models.CASCADE, related_name="products"
+    )
