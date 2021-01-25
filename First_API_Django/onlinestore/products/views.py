9ffdce245015468996b1870ace0d65996d7bb877
mechanic <furkalokbu@gmail.com>
2021-01-25 15:42:27 +0200

    works for day

--- First_API_Django/onlinestore/products/views.py
+++ First_API_Django/onlinestore/products/views.py
@@ -1,1 +1,12 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Product
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "products/product_detail.html"
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/product_list.html"
