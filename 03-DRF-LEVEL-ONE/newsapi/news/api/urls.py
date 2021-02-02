67422a70d111171cbd8e587ffd28725183b37c99
Serhii <furkalokbu@gmail.com>
2021-02-02 23:55:32 +0200

    finished 3 level

--- 03-DRF-LEVEL-ONE/newsapi/news/api/urls.py
+++ 03-DRF-LEVEL-ONE/newsapi/news/api/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from news.api.views import ArticleListCreateApiView, ArticleDetailApiView
+from news.api.views import (
+    ArticleListCreateApiView,
+    ArticleDetailApiView,
+    JournalistListCreateAPIView,
+)
 
 # from news.api.views import article_list_create_api_view, article_detail_api_view
 
 urlpatterns = [
     path("articles/", ArticleListCreateApiView.as_view(), name="article-list"),
     path("articles/<int:pk>", ArticleDetailApiView.as_view(), name="article-detail"),
+    path(
+        "journalists/", JournalistListCreateAPIView.as_view(), name="journalists-list"
+    ),
     # path("articles/", article_list_create_api_view, name="article-list"),
     # path("articles/<int:pk>", article_detail_api_view, name="article-detail"),
 ]
