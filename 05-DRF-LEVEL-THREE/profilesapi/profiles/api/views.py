c39f8f2947616226b143328265d3c9476b0acbc8
mechanic <furkalokbu@gmail.com>
2021-02-11 15:54:14 +0200

    test for project

--- 05-DRF-LEVEL-THREE/profilesapi/profiles/api/views.py
+++ 05-DRF-LEVEL-THREE/profilesapi/profiles/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import viewsets, mixins
@@ -29,13 +30,22 @@ class ProfileViewSet(
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated, IsOwnerProfileOrReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = ["city"]
 
 
 class ProfileStatusViewSet(ModelViewSet):
 
-    queryset = ProfileStatus.objects.all()
+    # queryset = ProfileStatus.objects.all()
     serializer_class = ProfileStatusSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        queryset = ProfileStatus.objects.all()
+        username = self.request.query_params.get("username", None)
+        if username is not None:
+            queryset = queryset.filter(user_profile__user__username=username)
+        return queryset
 
     def perform_create(self, serializer):
         user_profile = self.request.user.profile
