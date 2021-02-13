9086b086971b05569b01a28452d3431c63babb7b
Serhii <furkalokbu@gmail.com>
2021-02-13 15:41:44 +0200

    first lesson on Vue js

---
+++ 06-VUE-JS/03_First_Vue_Instance/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: "Hello World!",
+        value: 5,
+        imgSrc: "https://cdn.fishki.net/upload/post/201405/07/1267019/002.jpg",
+        link: "https://vuejs.org",
+    }
+})
