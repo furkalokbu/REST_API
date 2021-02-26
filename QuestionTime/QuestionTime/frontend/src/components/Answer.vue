36fc872cc57faa9e61b8d2364770a019a80a2e03
mechanic <furkalokbu@gmail.com>
2021-02-26 14:00:12 +0200

    answer botton

---
+++ QuestionTime/QuestionTime/frontend/src/components/Answer.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: "AnswerComponent",
+    props: {
+        answer: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
